7d5a3f10c1de4abe043e01291fb930cabd56f353
kris <krisd3214@gmail.com>
2024-08-22 13:46:13 +0700

    sec

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: 'D:/Program File (x86)/E2E-frontend/PresidentDemo-Web/tests',
+  testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
