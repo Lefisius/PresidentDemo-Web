ead445b9d11b9f289e509be0f7e25f43de0ba7df
its-Theerapat <theerapat1302@gmail.com>
2024-10-01 10:24:29 +0700

    ss

--- src/app/modules/president/E2E-Script/html.component.spec.ts
+++ src/app/modules/president/E2E-Script/html.component.spec.ts
@@ -8,6 +8,7 @@ test('test', async ({ page }) => {
   await page.getByPlaceholder('Search President').fill('Adams J');
   await page.getByPlaceholder('Search President').press('Enter');
   await page.getByRole('button', { name: 'Search' }).first().click();
+  await page.screenshot({ path: 'Errorอะไร.png' });
   await page.getByRole('cell', { name: 'Adams J', exact: true }).click();
   await page.getByRole('cell', { name: '1735' }).click();
   await page.getByPlaceholder('From').click();
